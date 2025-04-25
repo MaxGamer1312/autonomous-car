190f2e2563d39af2cdf7959488e04eabc07001d9
cbrt-1 <93866366+cbrt-1@users.noreply.github.com>
2025-04-25 16:59:27 -0500

    push

--- Assets/Tommy/Scripts/Classical Algorithm/HybridAstar.cs
+++ Assets/Tommy/Scripts/Classical Algorithm/HybridAstar.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Object = System.Object;
@@ -18,6 +19,7 @@ namespace Tommy.Scripts.Classical_Algorithm
         {
             Heap<State> openSet = new Heap<State>();
             HashSet<State> closedSet = new HashSet<State>();
+            HashSet<List<State>> results = new();
 
             State startingState = new State
             {
@@ -31,9 +33,28 @@ namespace Tommy.Scripts.Classical_Algorithm
             while (!openSet.IsEmpty())
             {
                 
-                if (iterations >= 3000)
+                if (iterations >= 5000)
                 {
-                    Debug.Log("Terminated Early");
+                    if (results.Any())
+                    {
+                        return results.OrderBy((a) =>
+                        {
+                            // float totalDistance = 0;
+                            // for (int i = 0; i < a.Count - 1; i++)
+                            // {
+                            //     float dx = a[i + 1].pose.x - a[i].pose.x;
+                            //     float dy = a[i + 1].pose.y - a[i].pose.y;
+                            //     totalDistance += Mathf.Sqrt(dx * dx + dy * dy);
+                            // }
+                            float totalCost = 0;
+                            foreach (var aState in a)
+                            {
+                                totalCost += aState.fCost;
+                            }
+                            return totalCost;
+                        }).FirstOrDefault();
+                    }
+                    
                     return null;
                 }
 
@@ -41,13 +62,13 @@ namespace Tommy.Scripts.Classical_Algorithm
                 
                 if (current.pose.DistanceTo(endPose) < 1f)
                 {
-                    Debug.Log("Found Path");
+                    results.Add(BackTrack(current));
                     return BackTrack(current);
                 }
                 closedSet.Add(current);
                 //map.WorldToCell(current.pose.x, current.pose.y) = true;
                 //ClassicalController.DebugCar(current.pose);
-                ClassicalController.DebugCar(current.pose, Color.red, 0f);
+                ClassicalController.DebugCar(current.pose, new Color(1, 0, 0, .5f), 0f);
                 foreach (var child in ChildStates(current, endPose))
                 {
                     if(map.OutOfBounds(child.pose.x, child.pose.y))
@@ -85,19 +106,21 @@ namespace Tommy.Scripts.Classical_Algorithm
             float maxSpeed = Mathf.Sqrt(tileWidth * tileWidth * 2) + .01f;
             float steeringAngle = 40 * Mathf.Deg2Rad;
             
-            (float driveDistance, float turningAngle, float cost)[] actions =
-            {
-                (-maxSpeed, -steeringAngle * .25f, 2f),
-                (-maxSpeed, 0, .5f),
-                (-maxSpeed, steeringAngle * .25f, 2f),
-                // (-maxSpeed, -steeringAngle, .02f),
-                // (-maxSpeed, 0, .0f),
-                // (-maxSpeed, steeringAngle, .02f),
-                (maxSpeed, -steeringAngle * .25f, 1f),
-                (maxSpeed, 0, 0f),
-                (maxSpeed, steeringAngle * .25f, 1f),
-            };
-            State[] result = new State[actions.Length];
+            
+            List<(float driveDistance, float turningAngle, float cost)> actions = new();
+
+            actions.Add((-maxSpeed, 0, .5f));
+            actions.Add((maxSpeed, 0, 0f));
+            
+            actions.Add((-maxSpeed, -steeringAngle * .25f, 2f));
+            actions.Add((-maxSpeed, steeringAngle * .25f, 2f));
+            
+            
+            actions.Add((maxSpeed, -steeringAngle * .25f, 2f));
+            actions.Add((maxSpeed, steeringAngle * .25f, 2f));
+            
+
+            State[] result = new State[actions.Count];
             for (int i = 0; i < result.Length; i++)
             {
                 
@@ -187,4 +210,26 @@ namespace Tommy.Scripts.Classical_Algorithm
             return pose.GetHashCode();
         }
     }
+    
+    public class ListComparer<T> : IEqualityComparer<List<T>>
+    {
+        public bool Equals(List<T> x, List<T> y)
+        {
+            if (x == null || y == null) return x == y;
+            return x.SequenceEqual(y);
+        }
+
+        public int GetHashCode(List<T> obj)
+        {
+            if (obj == null) return 0;
+
+            int hash = 17;
+            foreach (T item in obj)
+            {
+                hash = hash * 23 + (item == null ? 0 : item.GetHashCode());
+            }
+            return hash;
+        }
+    }
+
 }
--- Assets/Tommy/Scripts/Classical Algorithm/Map.cs
+++ Assets/Tommy/Scripts/Classical Algorithm/Map.cs
@@ -1,5 +1,4 @@
 using System;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace Tommy.Scripts.Classical_Algorithm
