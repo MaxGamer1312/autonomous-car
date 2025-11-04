7426818643036c210bc5d2def92d0d3ba31d367d
Shimal-Tajale <Shimal.Tajale@proton.me>
2025-11-03 18:41:26 -0600

    Update PigPioCarController.py
    Made sure you can actually import it to other programs, reintroduced local mode.

--- PigPioCarController.py
+++ PigPioCarController.py
@@ -9,10 +9,10 @@ SERVO_CENTER=1500
 
 class CarController:
     def __init__(self, drive_pin, direction_pin, turn_pin, ip= None, duty_cycle=0, min_mod=0, max_mod=255):
-        #if isinstance(ip, str):
-        self.pi = pigpio.pi(ip)  # Connect to remote Pi
-        #else:
-#            self.pi = pigpio.pi()    # Connect locally
+        if isinstance(ip, str):
+            self.pi = pigpio.pi(ip)  # Connect to remote Pi
+        else:
+            self.pi = pigpio.pi()    # Connect locally
 
         if not self.pi.connected:
             raise ConnectionError("Could not connect to pigpio daemon.")
@@ -73,51 +73,55 @@ class CarController:
         self.stop(self)
         self.pi.stop()
         #11 13 16 18 all seem to connect to the motor. Unsure which is direction, and which is drive.
-drive_pin=11 # pin for the motor contolling wheels
-direction_pin=13 # pin for the direction of the motor. not sure if needed.
-turn_pin=40 # pin for the servo
 
-command="" 
-#if input("ip?")[0]== "Y": 
-ip = input("Enter ip here : ")
-car= CarController(drive_pin, direction_pin, turn_pin,ip)
-#else :
- #   car= CarController(drive_pin, direction_pin, turn_pin)
-while True :
-    print("""Current commands : 
-          1. turn
-          2. accelerate
-          3. stop 
-          4. end
-          """)
-    command = int(input("Next command"))
-    if (command==1):
-        print("""
-              1. clockwise
-              2. counter clockwise
-              """)
-              
-        specfic = int(input("Which direction?"))
-        if specfic == 1 :
-            car.turn(1)
-        else :
-            car.turn(-1)
-    elif (command == 2): 
-        specfic = int(input("How fast?"))
-        if (specfic < 0):
-            car.accelerate()    
-        else :
-            car.accelerate(specfic)
-    elif (command == 3) :
-        car.stop()
-    else:
-        command = input("End connection?")
-        if command[0].upper()== "Y":
-            car.end_connection()
-            print("Connection terminated")
+if (__name__ == "__main__"):
+    drive_pin=11 # pin for the motor contolling wheels
+    direction_pin=13 # pin for the direction of the motor. not sure if needed.
+    turn_pin=40 # pin for the servo
+
+    command="" 
+    if input("Enter N if you want to connect locally?")[0] != "N": 
+        ip = input("Enter ip here : ")
+        car= CarController(drive_pin, direction_pin, turn_pin,ip)
+    else :
+       car= CarController(drive_pin, direction_pin, turn_pin)
+
+    # passing throttle number - how fast and turnign number,  
+    while True :
+        print("""Current commands : 
+            1. turn
+            2. accelerate
+            3. stop 
+            4. end
+            """)
+        command = int(input("Next command"))
+        if (command==1):
+            print("""
+                1. clockwise
+                2. counter clockwise
+                """)
+                
+            specfic = int(input("Which direction?"))
+            if specfic == 1 :
+                car.turn(1)
+            else :
+                car.turn(-1)
+        elif (command == 2): 
+            specfic = int(input("How fast?"))
+            if (specfic < 0):
+                car.accelerate()    
+            else :
+                car.accelerate(specfic)
+        elif (command == 3) :
+            car.stop()
+        else:
+            command = input("End connection?")
+            if command[0].upper()== "Y":
+                car.end_connection()
+                print("Connection terminated")
+            
+            
+
+
         
         
-
-
-    
-    
